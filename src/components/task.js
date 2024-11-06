cf94239f453939cfe23fdd03a226d46c93cc1640
Christo Leon <rnleon2000@gmail.com>
2024-11-06 17:28:21 +0800

    Update task.js

--- src/components/task.js
+++ src/components/task.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 const task = () => {
   return (
-    <div>
       <>
-        .container>.row.col-md-12>.card.card-white>.card-body
-        
+        <div className="container">
+          <div className="row col-md-12">
+            <div className="card card-white">
+              <div classname="card-body">
+                <h1>Hello world</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </>
-    </div>
   )
 }
 
